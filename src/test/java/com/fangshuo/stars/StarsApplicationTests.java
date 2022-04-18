75ed23743cc50eb633ff869de866848b4953125c
bigbigcarrot424 <941389381@qq.com>
2022-04-18 11:41:46 +0800

    1.1 初始化我的项目

---
+++ src/test/java/com/fangshuo/stars/StarsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fangshuo.stars;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
