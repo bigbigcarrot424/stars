738ff08f828ca07c19268dd1df55098f18cbf127
bigbigcarrot424 <941389381@qq.com>
2022-05-08 13:25:39 +0800

    21  前端获取好友列表

--- web/src/views/admin/MyFriend.vue
+++ web/src/views/admin/MyFriend.vue
@@ -1,24 +1,16 @@
 <template>
-
-    <a-card hoverable style="width: 300px">
-<!--        <template #cover>-->
-<!--            <img-->
-<!--                    alt="example"-->
-<!--                    src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png"-->
-<!--            />-->
-<!--        </template>-->
+    <a-card hoverable style="width: 300px" v-for="(item, index) in followList" :key="item.id">
         <template #actions>
             <home-outlined key="home" @click=""/>
             <message-outlined key="message" @click=""/>
             <delete-outlined key="delete" @click=""/>
         </template>
-        <a-card-meta title="Card title" description="This is the description">
+        <a-card-meta :title="item.name" :description="item.selfIntro">
             <template #avatar>
-                <a-avatar src="https://joeschmoe.io/api/v1/random" />
+                <a-avatar :src="SERVER + '/file/avatar/' + item.avatar" />
             </template>
         </a-card-meta>
     </a-card>
-
 </template>
 
 <script>
@@ -36,12 +28,23 @@
             HomeOutlined,
         },
         setup() {
+            const SERVER = process.env.VUE_APP_SERVER;
+            let followList = ref();
+            const getFollowList = () => {
+                axios.get(SERVER + "/follow/followList/" + store.state.user.id).then((response) => {
+                    const data = response.data;
+                    if (data){
+                        followList.value = data.content ? data.content :[];
+                    }
+                })
+            }
 
             onMounted(()=>{
-
+                getFollowList();
             })
             return {
-
+                SERVER,
+                followList,
             };
         },
     });
